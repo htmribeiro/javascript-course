71cc551dca280122827f15e963b2e716ba7c7ba8
Hamilton Ribeiro <htadeu@hotmail.com>
2019-06-19 23:22:49 -0300

    Add novo arquivo js da aula I01

---
+++ hcode/curso-javascript-basico/index.js
@@ -0,0 +1,23 @@
+var olaMundo = "Hello World!";
+
+console.log(olaMundo);
+/*
+let a = 10;
+const b = 20;
+
+console.log(a == b);
+//output > true
+*/
+
+/* let a = 10;
+const b = "10";
+
+console.log(a == b);
+//output > true 
+*/
+
+let a = 10;
+const b = "10";
+
+console.log(a === b);
+//output > false
